9223a279e756a48d1bba80b5f9ed3d5c16641bec
zebra314 <kidalfonso@gmail.com>
2026-07-18 21:24:06 +0800

    Add run pool env script

---
+++ scripts/run_pool_env.py
@@ -0,0 +1,46 @@
+import numpy as np
+from pathlib import Path
+from BalloonPoppingGymEnv.envs.pool_env import PoolEnv
+from BalloonPoppingGymEnv.evaluation.evaluate import load_pool_parameters
+from BalloonPoppingGymEnv.agents.itron_agent import ITronAgent
+from BalloonPoppingGymEnv.utils.setup_logging import setup_logging
+from BalloonPoppingGymEnv.utils.scene import Scene
+
+
+def run_for_development():
+    scenario_parameters, given_parameters = load_pool_parameters()
+    env = PoolEnv(render_mode='matplotlib', parameters=scenario_parameters)
+    agent = ITronAgent(given_parameters)
+    scene = Scene()
+
+    # Load the pool
+    script_dir = Path(__file__).resolve().parent
+    pool_file = script_dir / "pool_level_1_easy.npy"
+    trajectory_database = np.load(pool_file, mmap_mode='r')
+    pool_capacity = trajectory_database.shape[0]
+
+    # Sample tracks from the pool
+    num_balloons = scenario_parameters["balloon"]["num"]
+    sampled_indices = np.random.choice(pool_capacity, size=num_balloons, replace=False)
+    extracted_tracks = np.array(trajectory_database[sampled_indices])
+
+    env.update_source_trajectories(extracted_tracks)
+
+    observation, info = env.reset(seed=scenario_parameters["scenario"]["random_seed"])
+    terminated = False
+
+    while not terminated:
+        action = agent.get_action(observation)
+        observation, reward, terminated, _, info = env.step(action)
+        scene.update(observation, info)
+
+        if info['popped_count'] == num_balloons:
+            print(f"\nAll balloons popped at simulation_time: {observation['simulation_time']:.2f} sec")
+            break
+
+    scene.draw()
+
+
+if __name__ == "__main__":
+    setup_logging()
+    run_for_development()
