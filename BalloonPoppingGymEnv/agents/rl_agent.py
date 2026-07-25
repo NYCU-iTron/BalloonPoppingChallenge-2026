128d36c271669e581b99bbbe2ed7954b0e7a0caf
zebra314 <kidalfonso@gmail.com>
2026-07-25 16:38:06 +0800

    Update agent workflow

--- BalloonPoppingGymEnv/agents/rl_agent.py
+++ BalloonPoppingGymEnv/agents/rl_agent.py
@@ -1,4 +1,3 @@
-import logging
 import numpy as np
 from pathlib import Path
 
@@ -11,9 +10,12 @@ from BalloonPoppingGymEnv.utils.rl_utils import RL_FRAME_SKIP
 
 
 class RLAgent(BaseAgent):
-    def __init__(self, given_parameters, model_path: str = None, vecnormalize_path: str = None):
+    def __init__(self,
+                 given_parameters,
+                 model_path: Path | None = None,
+                 vecnormalize_path: Path | None = None,
+                ):
         super().__init__(given_parameters)
-        self.logger = logging.getLogger(__name__)
 
         if model_path is None:
             model_path = Path(__file__).resolve().parent / "model.zip"
@@ -31,13 +33,9 @@ class RLAgent(BaseAgent):
 
         # Commands
         self.should_launch = None
-        self.rl_action = None
-        self.a_cmd = None
-        self.throttle = None
-
-        # States
-        self.rocket_state = None
-        self.target_state = None
+        self.launch_inclination_heading = None
+        self.desired_acc = None
+        self.desired_throttle = None
 
     def reset(self) -> None:
         self.estimator.reset()
@@ -49,40 +47,39 @@ class RLAgent(BaseAgent):
 
         # Commands
         self.should_launch = None
-        self.rl_action = None
-        self.a_cmd = None
-        self.throttle = None
-
-        # States
-        self.rocket_state = None
-        self.target_state = None
+        self.launch_inclination_heading = None
+        self.desired_acc = None
+        self.desired_throttle = None
 
     def get_action(self, observation: dict) -> dict:
-        if not self.should_launch or self.should_launch is None:
+        # Idle state
+        if not self.should_launch:
             self.should_launch = self.selector.should_launch(observation)
+
+            # Still in idle state
+            if not self.should_launch:
+                return {
+                    "launch": False,
+                    "launch_inclination_heading": np.array([90.0, 0.0]),
+                    "tvc": np.zeros(2, dtype=np.float64),
+                    "roll": 0.0,
+                    "throttle": 0.0,
+                }
+
+            # Run get_launch_heading only once after should_launch turn true
             self.launch_inclination_heading = self.selector.get_launch_heading(observation)
 
-        if not self.should_launch:
-            return {
-                "launch": False,
-                "launch_inclination_heading": np.zeros(2, dtype=np.float64),
-                "tvc": np.zeros(2, dtype=np.float64),
-                "roll": 0.0,
-                "throttle": 0.0,
-            }
-
-        self.rocket_state = self.estimator.estimate_rocket(observation)
+        rocket_state = self.estimator.estimate_rocket(observation)
 
         if self.skip_counter % RL_FRAME_SKIP == 0:
             balloon_states = self.estimator.predict_balloons(observation)
-            target_idx = self.selector.select_target(balloon_states, self.rocket_state)
-            self.target_state = self.estimator.predict_target(observation, target_idx)
-            self.a_cmd, self.throttle = self.navigator.compute(self.target_state, self.rocket_state)
+            target_idx = self.selector.select_target(balloon_states, rocket_state)
+            target_state = self.estimator.predict_target(observation, target_idx)
+            self.desired_acc, self.desired_throttle = self.navigator.compute(target_state, rocket_state)
 
         self.skip_counter += 1
-        self.rl_action = self.navigator.last_residual.copy()
 
-        tvc, roll, throttle = self.controller.compute(self.rocket_state, self.a_cmd, self.throttle)
+        tvc, roll, throttle = self.controller.compute(rocket_state, self.desired_acc, self.desired_throttle)
 
         return {
             "launch": self.should_launch,
