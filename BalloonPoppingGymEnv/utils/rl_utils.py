b37ddcca3ce80f0d31e727752239195a9b46451e
zebra314 <kidalfonso@gmail.com>
2026-07-28 01:07:42 +0800

    Fix cycle import bug

--- BalloonPoppingGymEnv/utils/rl_utils.py
+++ BalloonPoppingGymEnv/utils/rl_utils.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-from BalloonPoppingGymEnv.envs.pool_env import PoolEnv
-from BalloonPoppingGymEnv.envs.rl_navigator_env import RLNavigatorEnv
-
 RL_FRAME_SKIP = 5
 MAX_ACC = 20.0
 
 def make_custom_env(scenario_params, given_params, pool_path):
     def _init():
+        from BalloonPoppingGymEnv.envs.rl_navigator_env import RLNavigatorEnv
+        from BalloonPoppingGymEnv.envs.pool_env import PoolEnv
         raw_env = PoolEnv(render_mode=None, parameters=scenario_params)
         return RLNavigatorEnv(raw_env, given_params, pool_path)
     return _init
