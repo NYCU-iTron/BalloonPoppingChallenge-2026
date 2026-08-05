b93887a979f04efc35aa8cb72f90420a5cc5e554
zebra314 <kidalfonso@gmail.com>
2026-08-05 17:32:24 +0800

    Update e2e

--- BalloonPoppingGymEnv/envs/e2e_static_env.py
+++ BalloonPoppingGymEnv/envs/e2e_static_env.py
@@ -1,63 +1,39 @@
 import gymnasium as gym
 
 from BalloonPoppingGymEnv.utils.schema import Schema
-from BalloonPoppingGymEnv.agents.gnc.selector import Selector
-from BalloonPoppingGymEnv.agents.gnc.estimator import Estimator
 from BalloonPoppingGymEnv.utils.reward_calculator import RewardCalculator
 from BalloonPoppingGymEnv.utils.e2e_utils import (
     action_space,
     observation_space,
-    compute_rl_observation,
+    launch_schedule,
+    scale_rl_action,
 )
 
 class E2EStaticEnv(gym.Wrapper):
     def __init__(self, env: gym.Env, given_parameters):
         super().__init__(env)
 
-        self.selector = Selector(given_parameters)
-        self.estimator = Estimator(given_parameters)
+        self.env = env
+        self.num_balloons = 1
         self.reward_calculator = RewardCalculator(given_parameters)
 
         self.action_space = action_space
         self.observation_space = observation_space
 
     def reset(self, seed=None, options=None):
-        self.estimator.reset()
-        self.selector.reset()
+        # Update balloon pos
+        # self.env.update_balloons
+
         self.reward_calculator.reset()
-
-        self.rocket_state = None
-        self.launch_inclination_heading = None
-
-        observation, info = self.env.reset(seed=seed, options=options)
-
-        self.launch_inclination_heading = self.selector.get_launch_heading(observation)
-
-        # Update states
-        simulation_time = observation[Schema.Observation.SIMULATION_TIME]
-
-        # Get target state
-        raw_balloon_states = observation[Schema.Observation.BALLOON_STATES]
-        target_state = self.selector.get_target_state(
-            balloon_states=raw_balloon_states,
-            target_idx=target_idx,
-        )
-
-        rl_obs = compute_rl_observation(
-            rocket_state=self.rocket_state,
-            target_state=target_state,
-        )
+        self.env.reset(seed=seed, options=options)
+        rl_obs, info = launch_schedule(self.env)
 
         return rl_obs, info
 
     def step(self, rl_action):
         rl_reward = 0.0
         pop_count = 0.0
-        desired_acc = scale_rl_action(rl_action)
-
-        tvc, roll, throttle = self.controller.compute(
-            desired_acc=desired_acc,
-        )
+        tvc, roll, throttle = scale_rl_action(rl_action)
 
         action = {
             "launch": True,
@@ -71,12 +47,7 @@ class E2EStaticEnv(gym.Wrapper):
         pop_count += reward
 
         # Update states
-        self.rocket_state = self.estimator.estimate_rocket(observation)
         simulation_time = observation[Schema.Observation.SIMULATION_TIME]
-        self.controller.update(
-            rocket_state=self.rocket_state,
-            simulation_time=simulation_time,
-        )
 
         # Check truncated
         if self.controller.burnout and not (terminated or truncated):
@@ -88,23 +59,9 @@ class E2EStaticEnv(gym.Wrapper):
             truncated = True
 
         # Select target
-        pred_balloon_states = self.estimator.predict_balloons(observation)
-        target_idx = self.selector.select_target(
-            balloon_states=pred_balloon_states,
-            rocket_state=self.rocket_state,
-        )
 
         # Get target state
         raw_balloon_states = observation[Schema.Observation.BALLOON_STATES]
-        target_state = self.selector.get_target_state(
-            balloon_states=raw_balloon_states,
-            target_idx=target_idx,
-        )
-
-        rl_obs = compute_rl_observation(
-            rocket_state=self.rocket_state,
-            target_state=target_state,
-        )
 
         rl_reward, rl_reward_dict = self.reward_calculator.compute(
             observation=observation,
--- BalloonPoppingGymEnv/utils/e2e_utils.py
+++ BalloonPoppingGymEnv/utils/e2e_utils.py
@@ -1,5 +1,6 @@
-from gymnasium import spaces
 import numpy as np
+from gymnasium import spaces
+import gymnasium as gym
 
 from BalloonPoppingGymEnv.utils.schema import Schema
 
@@ -38,9 +39,72 @@ observation_space = spaces.Box(
     dtype=np.float32
 )
 
-def compute_rl_observation(rocket_state: np.ndarray, target_state: np.ndarray) -> np.ndarray:
+def scale_rl_action(normalized_action: np.ndarray) -> tuple[np.ndarray, float, float]:
+    max_roll_torque = 10
+    gimbal_range = 15
+    throttle_range = [0.0, 1.0]
+
+    roll = normalized_action[0] * max_roll_torque
+    tvc = normalized_action[1:3] * gimbal_range
+
+    return tvc, roll, throttle
+
+def launch_schedule(env: gym.Env, rl_observator: RLObservator):
+    # while ...:
+    #     action = {
+    #         "launch": True,
+    #         "launch_inclination_heading": launch_inclination_heading,
+    #         "tvc": tvc,
+    #         "roll": roll,
+    #         "throttle": throttle,
+    #     }
+
+    #     observation, _, _, _, info = env.step(action)
+    #     rl_observator.update(observation)
+
+    # rl_obs = rl_observator.get_observation()
+    # return rl_obs, info
     ...
 
+class RLObservator:
+    def __init__(self, given_parameters):
+        # Time step
+        self.sampling_rate = given_parameters[Schema.Given.Section.ROCKET][Schema.Given.Rocket.SENSORS][Schema.Given.Sensors.SAMPLING_RATE]
+        self.dt = 1.0 / self.sampling_rate
 
-def scale_rl_action(normalized_action: np.ndarray) -> np.ndarray:
-    ...
+
+    def update(self, observation: dict) -> None:
+        rocket_sensors = observation[Schema.Observation.ROCKET_SENSORS]
+
+        # Parse sensor data
+        rocket_gyro = rocket_sensors[0:3] # gyroscopes
+        rocket_acc = rocket_sensors[3:6] # accelerometers
+        rocket_pos = rocket_sensors[6:9] # GNSS position
+        rocket_vel = rocket_sensors[9:12] # GNSS velocity
+
+        delta_theta = rocket_gyro * self.dt
+        theta_mag = np.linalg.norm(delta_theta)
+
+        if theta_mag > 1e-8:
+            # Generate delta rotation quaternion
+            qw_d = np.cos(theta_mag / 2.0)
+            qxyz_d = (delta_theta / theta_mag) * np.sin(theta_mag / 2.0)
+            q_delta = np.array([qw_d, qxyz_d[0], qxyz_d[1], qxyz_d[2]])
+
+            # Perform quaternion multiplication
+            # quat = quat x q_delta
+            qw, qx, qy, qz = self.rocket_quat
+            dw, dx, dy, dz = q_delta
+
+            new_qw = qw * dw - qx * dx - qy * dy - qz * dz
+            new_qx = qw * dx + qx * dw + qy * dz - qz * dy
+            new_qy = qw * dy - qx * dz + qy * dw + qz * dx
+            new_qz = qw * dz + qx * dy - qy * dx + qz * dw
+
+            self.rocket_quat = np.array([new_qw, new_qx, new_qy, new_qz])
+
+            # Normalize to eliminate compounding numerical drift errors
+            self.rocket_quat /= np.linalg.norm(self.rocket_quat)
+
+    def get_observation(self) -> np.ndarray:
+        ...
